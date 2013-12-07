2c6b636af2b1800630ab6195b85add10042634a4
Dario Seminara <darios3@gmail.com>
2013-12-07 20:24:25 -0300

    +5 test pass: literal objects with one property

--- lib/dsjsrb/processor.rb
+++ lib/dsjsrb/processor.rb
@@ -15,17 +15,22 @@ module DSJSRB
             s(:str, eval(tree[2]))
           when :resolve
             s(:call, s(:lvar, :global_scope), :get_attribute, s(:lit, tree[2].to_sym))
+          when :op_equal
+            s(:call, s(:call, nil, :global_scope), :set_attribute, s(:lit, tree[2][1].to_sym), process(tree[3]))
           else
             raise "unrecognized expression type #{tree[1]}"
         end
       when :source_elements
         process(tree[-1])
-      when :block
+      when :number
+        s(:lit, tree[1])
+      when :object_literal
+            p tree
         s(:iter, 
           s(:call, 
             s(:call, s(:const, :JSObject), :new), :tap), 
               s(:args, :obj), 
-              s(:call, s(:lvar, :obj), :set_attribute, s(:lit, tree[2][1]), process(tree[2][2])))
+              s(:call, s(:lvar, :obj), :set_attribute, s(:lit, tree[1][1]), process(tree[1][2])))
       else
         raise "unrecognize node type #{tree[0]}"
       end
