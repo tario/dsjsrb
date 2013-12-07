ad2efae5f6c5c47dd7fb13a0b666b4895ed48699
Dario Seminara <darios3@gmail.com>
2013-12-07 18:35:23 -0300

    +3 failing test: object literals with different variable names and values

--- spec/jsobject_spec.rb
+++ spec/jsobject_spec.rb
@@ -11,5 +11,20 @@ describe DSJSRB do
       obj = js_context.eval_expr('{a:1}')
       obj.get_attribute(:a).should be == 1.to_f
     end
+
+    it "should parse object with one attribute" do
+      obj = js_context.eval_expr('{b:1}')
+      obj.get_attribute(:b).should be == 1.to_f
+    end
+
+    it "should parse object with one attribute" do
+      obj = js_context.eval_expr('{a:2}')
+      obj.get_attribute(:a).should be == 2.to_f
+    end
+
+    it "should parse object with one attribute" do
+      obj = js_context.eval_expr('{b:2}')
+      obj.get_attribute(:b).should be == 2.to_f
+    end
   end
 end
