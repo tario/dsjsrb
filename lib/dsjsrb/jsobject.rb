08f4bfc41dc01c24689b826e2e0c7bfe71b19ba5
Dario Seminara <darios3@gmail.com>
2013-12-07 18:12:13 -0300

    +2 test pass: Scope#define_attribute

--- lib/dsjsrb/jsobject.rb
+++ lib/dsjsrb/jsobject.rb
@@ -1,19 +1,38 @@
 module DSJSRB
 class JSObject
-  def initialize
-    @hash = Hash.new
+  class << self
+    alias :original_new :new
+    def new
+      Class.new(JSObject).original_new
+    end
   end
 
   def set_attribute(attr_name, value)
-    @hash[attr_name] = value
+    unless respond_to?(attr_name)
+      define_attribute(attr_name, value)
+    else
+      method(attr_name).owner.class_eval do
+        define_method(attr_name) do
+          value
+        end
+      end
+    end
   end
 
   def get_attribute(attr_name)
-    @hash[attr_name]
+    send(attr_name)
+  end
+
+  def define_attribute(attr_name, value)
+    self.class.class_eval do
+      define_method(attr_name) do
+        value
+      end
+    end
   end
 
   def create_scope
-    dup
+    Class.new(self.class).original_new
   end
 end
 end
