1cd103e219d80230d3aadfd43a553143539f68d9
Dario Seminara <darios3@gmail.com>
2013-12-07 18:43:06 -0300

    +2 failing test: object literals with two attributes

--- spec/jsobject_spec.rb
+++ spec/jsobject_spec.rb
@@ -7,24 +7,40 @@ describe DSJSRB do
   end
 
   describe "literal object expressions" do
-    it "should parse object with one attribute" do
-      obj = js_context.eval_expr('{a:1}')
-      obj.get_attribute(:a).should be == 1.to_f
+    context "with one attribute" do
+      it "should parse object" do
+        obj = js_context.eval_expr('{a:1}')
+        obj.get_attribute(:a).should be == 1.to_f
+      end
+
+      it "should parse object" do
+        obj = js_context.eval_expr('{b:1}')
+        obj.get_attribute(:b).should be == 1.to_f
+      end
+
+      it "should parse object" do
+        obj = js_context.eval_expr('{a:2}')
+        obj.get_attribute(:a).should be == 2.to_f
+      end
+
+      it "should parse object" do
+        obj = js_context.eval_expr('{b:2}')
+        obj.get_attribute(:b).should be == 2.to_f
+      end
     end
 
-    it "should parse object with one attribute" do
-      obj = js_context.eval_expr('{b:1}')
-      obj.get_attribute(:b).should be == 1.to_f
-    end
+    context "with two attributes" do
+      it "should parse object" do
+        ctx = js_context
+        obj = ctx.eval_expr('obj = {a: 1, b: 2}')
+        ctx.global_scope.get_attribute(:obj).get_attribute(:a).should be == 1.to_f
+      end
 
-    it "should parse object with one attribute" do
-      obj = js_context.eval_expr('{a:2}')
-      obj.get_attribute(:a).should be == 2.to_f
-    end
-
-    it "should parse object with one attribute" do
-      obj = js_context.eval_expr('{b:2}')
-      obj.get_attribute(:b).should be == 2.to_f
+      it "should parse object" do
+        ctx = js_context
+        obj = ctx.eval_expr('obj = {a: 1, b: 2}')
+        ctx.global_scope.get_attribute(:obj).get_attribute(:b).should be == 2.to_f
+      end
     end
   end
 end
