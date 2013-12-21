f8c5db2fe9b8e3c31bd093bf62783bd7378093d5
Dario Seminara <darios3@gmail.com>
2013-12-21 12:24:02 -0300

    refactor: moved expresion parsing to process_expresion, and let statement parsing on process, +1 test pass: functions with one argument are now callable

--- lib/dsjsrb/processor.rb
+++ lib/dsjsrb/processor.rb
@@ -8,7 +8,7 @@ module DSJSRB
           s(:lit, tree[1])
         when :array
           s(:array).tap do |s|
-            tree[1..-1].map(&method(:process)).each(&s.method(:<<))
+            tree[1..-1].map(&method(:process_expresion)).each(&s.method(:<<))
           end
         when :string
           s(:str, eval(tree[1]))
@@ -17,23 +17,40 @@ module DSJSRB
         when :op_equal
           case tree[1][0]
           when :resolve
-            s(:call, s(:call, nil, :current_scope), :set_attribute, s(:lit, tree[1][1].to_sym), process(tree[2]))
+            s(:call, s(:call, nil, :current_scope), :set_attribute, s(:lit, tree[1][1].to_sym), process_expresion(tree[2]))
           when :dot_accessor
             s(:call,
               process_expresion(tree[1][1..-2]),
               :set_attribute,
               s(:lit, tree[1][-1].to_sym),
-              process(tree[2])
+              process_expresion(tree[2])
               )
           else
             raise "unrecognized accessor type #{tree[1][0]}"
           end
+        when :object_literal
+          assignation_block = s(:block)
+          tree[1..-1].each do |subtree|
+            assignation_block << s(:call, s(:lvar, :obj), :set_attribute, s(:lit, subtree[1]), process_expresion(subtree[2]))
+          end
+
+          s(:iter, 
+            s(:call, 
+              s(:call, s(:const, :JSObject), :new), :tap), 
+                s(:args, :obj),
+                assignation_block)
+        when :null
+          s(:nil)
         when :dot_accessor
           s(:call,
             process_expresion(tree[1..-2]),
             :get_attribute,
             s(:lit, tree[-1].to_sym)
             )
+        when :function_expr
+          s(:iter, s(:call, nil, :lambda), s(:args), process(tree[-1]))
+        when :element
+          process_expresion(tree[1..-1])
         else
           raise "unrecognized expression type #{tree[0]}"
       end
@@ -41,32 +58,17 @@ module DSJSRB
 
     def process(tree)
       case tree[0]
-      when :element, :expression_statement
-        process_expresion(tree[1..-1])
+      when :expression_statement
+          process_expresion(tree[1..-1])
       when :source_elements
         process(tree[-1])
       when :number
         s(:lit, tree[1])
-      when :object_literal
-        assignation_block = s(:block)
-        tree[1..-1].each do |subtree|
-          assignation_block << s(:call, s(:lvar, :obj), :set_attribute, s(:lit, subtree[1]), process(subtree[2]))
-        end
-
-        s(:iter, 
-          s(:call, 
-            s(:call, s(:const, :JSObject), :new), :tap), 
-              s(:args, :obj),
-              assignation_block)
-      when :null
-        s(:nil)
       when :return
-        s(:next, process(s(*tree[1..-1])))
+        s(:next, process_expresion(s(*tree[1..-1])))
       when :function_body
         subtree = tree[2]
         subtree ? process(subtree) : s(:nil)
-      when :function_expr
-        s(:iter, s(:call, nil, :lambda), s(:args), process(tree[-1]))
       else
         raise "unrecognize node type #{tree[0]}"
       end
