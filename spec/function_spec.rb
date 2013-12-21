8ed582b44350e3ebf876d4055020ce2a8abc9bad
Dario Seminara <darios3@gmail.com>
2013-12-21 11:44:21 -0300

    +2 test pass: function returning null

--- spec/function_spec.rb
+++ spec/function_spec.rb
@@ -4,14 +4,31 @@ describe DSJSRB::JSFunction do
   let :js_context do
     DSJSRB::Context.new
   end
+  
+  shared_examples_for "a callable object" do
+    it "should respond to call" do
+      should respond_to(:call)
+    end
+  end
 
   describe "empty function" do
     subject do
       js_context.eval_expr("f = function(){}")
+      js_context.global_scope.get_attribute(:f)
     end
 
-    it "should respond to call" do
-      should respond_to(:call)
+    it_behaves_like "a callable object"
+  end
+
+  describe "function returning null" do
+    subject do
+      js_context.eval_expr("f = function(){return null; }")
+      js_context.global_scope.get_attribute(:f)
+    end
+
+    it_behaves_like "a callable object"
+    it "should return nil" do
+      subject.call.should be == nil
     end
   end
 end
